ee099b3d8a0558f5f89360163871197f17ce0cf7
whipm <whipmiester@hotmail.com>
2024-07-19 12:20:27 -0400

    autogenerated id

--- StoreFront/src/main/java/NateGroup/StoreFront/Products/Product.java
+++ StoreFront/src/main/java/NateGroup/StoreFront/Products/Product.java
@@ -1,14 +1,20 @@
 package NateGroup.StoreFront.Products;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     Long price;
     int quantity;
     String color;
     String size;
+    String name;
 }
