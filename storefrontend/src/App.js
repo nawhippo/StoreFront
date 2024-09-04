3c0d8faa4764c8f4fbbaa0cbd2b9c8d2084b595b
whipm <whipmiester@hotmail.com>
2024-09-04 19:06:57 -0400

    added paypal integration

--- storefrontend/src/App.js
+++ storefrontend/src/App.js
@@ -1,40 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { useState } from 'react';
-import ProductPage from "../ProductPage/ProductPage.js";
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import 'bootstrap/dist/css/boostrap.min.css';
+import Home from './Home';
+import Contact from './Contact';
 
-function App = () => {
-  const [userState, setUserState] = useState(null)
-  const userContext = useUserContext();
-  url = "localhost:8080/"
+function App() {
   return (
     <Router>
-    <div className="App">
-      <nav className="navbar" >
-     <li className="nav-item">
-        <a className="nav-link" href={`${url}/Products`}>Products</a>
-      </li> 
-      userContext ? (
-      <li className="nav-item">
-        <a className="nav-link" href={`${url}/Account`}>Account</a>
-      </li>
-      ) : (
-        null
-      )
-
-      <li class="nav-item">
-        <a class="nav-link" href="#">Orders</a>
-      </li>
-      </nav>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/Products" component={ProductPage} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
-    </div>
-</Router>
-);
+      <div className="App">
+        <nav className="navbar">
+          <li className="nav-item">
+            <a className="nav-link" href="/Products">Products</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/ShoppingCart">Orders</a>
+          </li>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Products" element={<ProductPage />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
