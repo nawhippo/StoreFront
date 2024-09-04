bf105f9bb656dd92436ea1765945929096bf9730
whipm <whipmiester@hotmail.com>
2024-09-04 19:08:16 -0400

    added paypal integration

---
+++ StoreFront/src/main/java/NateGroup/StoreFront/Orders/ClientTokenResponse.java
@@ -0,0 +1,37 @@
+package NateGroup.StoreFront.Orders;
+
+public class ClientTokenResponse {
+    private String clientToken;
+    private String clientId;
+    private String paypalSdkBaseUrl;
+
+    public ClientTokenResponse(String clientId, String clientToken, String paypalSdkBaseUrl) {
+        this.clientId = clientId;
+        this.clientToken = clientToken;
+        this.paypalSdkBaseUrl = paypalSdkBaseUrl;
+    }
+
+    public String getClientToken() {
+        return clientToken;
+    }
+
+    public void setClientToken(String clientToken) {
+        this.clientToken = clientToken;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getPaypalSdkBaseUrl() {
+        return paypalSdkBaseUrl;
+    }
+
+    public void setPaypalSdkBaseUrl(String paypalSdkBaseUrl) {
+        this.paypalSdkBaseUrl = paypalSdkBaseUrl;
+    }
+}
