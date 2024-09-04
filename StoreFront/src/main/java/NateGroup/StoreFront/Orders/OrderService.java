ce0c6f8b702518ca9801605095413619e21ab94f
whipm <whipmiester@hotmail.com>
2024-09-04 19:09:11 -0400

    added paypal integration

--- StoreFront/src/main/java/NateGroup/StoreFront/Orders/OrderService.java
+++ StoreFront/src/main/java/NateGroup/StoreFront/Orders/OrderService.java
@@ -1,30 +1,66 @@
 package NateGroup.StoreFront.Orders;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class OrderService {
 
-    @Autowired
-    private OrderRepository orderRepository;
+    private final Dotenv dotenv;
 
-    public List<Order> getAllOrders() {
-        return orderRepository.findAll();
+    private final RestClient restClient;
+
+    private final String PAYPAL_API_BASE_URL;
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(RestClient.Builder restClientBuilder, OrderRepository orderRepository) {
+        this.dotenv = Dotenv.load();
+
+        this.PAYPAL_API_BASE_URL = this.dotenv.get("PAYPAL_API_BASE_URL", "https://api-m.sandbox.paypal.com");
+
+        this.restClient = restClientBuilder.baseUrl(this.PAYPAL_API_BASE_URL).build();
+        this.orderRepository = orderRepository;
     }
 
-    public Optional<Order> getOrderById(long id) {
-        return orderRepository.findById(id);
+    public ResponseEntity<Map<String, Object>> createOrder(String accessToken, Map<String, Object> payload) {
+        ResponseEntity<Map<String, Object>> result
+                = this.restClient.post()
+                .uri("/v2/checkout/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(header -> {
+                    header.add("PayPal-Request-Id", Long.toString(System.currentTimeMillis()));
+                    header.add("Authorization", "Bearer " + accessToken);
+                })
+                .body(payload)
+                .retrieve()
+                .toEntity(new ParameterizedTypeReference<Map<String, Object>>() {
+                });
+
+        return result;
     }
 
-    public Order createOrder(Order order) {
-        return orderRepository.save(order);
+    public ResponseEntity<Order> getAllOrders() {
+        ResponseEntity<Order> result = (ResponseEntity<Order>) orderRepository.getAllOrders();
+        return result;
     }
 
-    public void deleteOrder(long id) {
-        orderRepository.deleteById(id);
+    public ResponseEntity<Order> deleteOrder(Long Id){
+        Order order = orderRepository.getById(Id);
+        orderRepository.delete(order);
+        return ResponseEntity.ok(order);
+    }
+    public ResponseEntity<Order> getOrderById(Long Id){
+        Order order = orderRepository.getById(Id);
+        return ResponseEntity.ok(order);
     }
 }
