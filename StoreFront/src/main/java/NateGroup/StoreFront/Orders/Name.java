fa911a808b8e7acdd53784d502323eeaa53a8b9e
whipm <whipmiester@hotmail.com>
2024-09-04 19:08:31 -0400

    added paypal integration

---
+++ StoreFront/src/main/java/NateGroup/StoreFront/Orders/Name.java
@@ -0,0 +1,14 @@
+package NateGroup.StoreFront.Orders;
+
+public class Name {
+
+    private String fullName;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
