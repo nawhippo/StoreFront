4a1c9d8e0e2c3b7a5d128f004957966b3fde7dc4
whipm <whipmiester@hotmail.com>
2024-09-04 19:10:56 -0400

    added paypal integration

---
+++ StoreFront/src/main/java/NateGroup/StoreFront/Orders/PaymentToken.java
@@ -0,0 +1,13 @@
+package NateGroup.StoreFront.Orders;
+
+public class PaymentToken {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
