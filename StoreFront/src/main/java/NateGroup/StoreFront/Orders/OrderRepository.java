0db3af8c6b747859cc952f49506eb639d3231975
whipm <whipmiester@hotmail.com>
2024-09-04 19:08:59 -0400

    added paypal integration

--- StoreFront/src/main/java/NateGroup/StoreFront/Orders/OrderRepository.java
+++ StoreFront/src/main/java/NateGroup/StoreFront/Orders/OrderRepository.java
@@ -2,8 +2,12 @@ package NateGroup.StoreFront.Orders;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Optional<Order> findByCustomerId(Long customerId);
+    List<Order> getAllOrders();
+
+    Optional<Order> getOrder();
 }
